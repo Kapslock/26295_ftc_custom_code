c6877aaeeb1cbf00e504d624d818096c33b509ca
ksmlau <kirbylau0@gmail.com>
2025-09-02 15:19:25 -0400

    added starter code for 2025-2026 season

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tests/SampleTeleOpMode.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.opmodes.tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Hardware;
+import org.firstinspires.ftc.teamcode.subsystems.mecanum.MecanumCommand;
+
+
+@TeleOp(name = "TeleopSample", group = "TeleOp")
+public class SampleTeleOpMode extends LinearOpMode {
+
+    // opmodes should only own commands
+    private MecanumCommand mecanumCommand;
+    private ElapsedTime timer;
+
+    private ElapsedTime resetTimer;
+
+    private Hardware hw;
+
+    enum ROBOT_STATE{
+        IDLE, SLOW
+    }
+
+    public ROBOT_STATE liftState = ROBOT_STATE.IDLE;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        hw = Hardware.getInstance(hardwareMap);
+        mecanumCommand = new MecanumCommand(hw);
+        while (opModeInInit()){
+            telemetry.update();
+        }
+
+        // Wait for start button to be pressed
+        waitForStart();
+
+        // Loop while OpMode is running
+        while (opModeIsActive()) {
+            mecanumCommand.handleMovement(
+                    gamepad1.left_stick_y,
+                    gamepad1.left_stick_x,
+                    gamepad1.right_stick_x
+            );
+
+            processTelemetry();
+
+            if (gamepad1.start){
+                mecanumCommand.resetPinPointOdometry();
+            }
+        }
+
+    }
+    public void processTelemetry(){
+        //add telemetry messages here
+        telemetry.addData("resetTimer: ",  resetTimer.milliseconds());
+        telemetry.addLine("---------------------------------");
+
+        telemetry.update();
+    }
+}
