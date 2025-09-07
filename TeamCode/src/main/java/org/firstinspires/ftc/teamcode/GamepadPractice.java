424e3461ae7310f347bf16b959d55778588a1ea4
AuroraFTC23295 <harry.wiencek@gmail.com>
2025-09-06 22:10:58 -0700

    Gamepad Tutorial

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamepadPractice.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+@Disabled
+@TeleOp
+public class GamepadPractice extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("x", gamepad1.left_stick_x);
+        telemetry.addData("x", gamepad1.left_stick_y);
+        telemetry.addData("a", gamepad1.a);
+    }
+}
