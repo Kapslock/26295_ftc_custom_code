190909426eb9c5f9e0adfa56db1bf29c63df4b1a
BISHER-AL-MASRI <bbgyttv@gmail.com>
2025-09-16 10:47:19 -0400

    crack

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestOp.java
@@ -3,36 +3,34 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.util.ElapsedTime;
-import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
-import java.util.Locale;
 
-@TeleOp(name="Final code", group="Linear OpMode")
+@TeleOp(name="TestOp", group="Linear OpMode")
 public class TestOp extends LinearOpMode {
-    private LinearOpMode myOpMode = null; // gain access to methods in the calling OpMode.
+
     private DcMotor skibidi = null;
     private DcMotor skibidii = null;
     private DcMotor skibidiii = null;
     private DcMotor skibidiiii = null;
+
     @Override
     public void runOpMode() {
-        skibidi = myOpMode.hardwareMap.get(
-                DcMotor.class,
-                "left_front_drive"
-        );
-        skibidii = myOpMode.hardwareMap.get(DcMotor.class, "left_back_drive");
+        skibidi = hardwareMap.get(DcMotor.class, "left_front_drive");
+        skibidii = hardwareMap.get(DcMotor.class, "left_back_drive");
+        skibidiii = hardwareMap.get(DcMotor.class, "right_front_drive");
+        skibidiiii = hardwareMap.get(DcMotor.class, "right_back_drive");
 
-        skibidiii = myOpMode.hardwareMap.get(
-                DcMotor.class,
-                "right_front_drive"
-        );
-        skibidiiii = myOpMode.hardwareMap.get(DcMotor.class, "right_back_drive");
-
-        waitForStart();
 
         skibidi.setPower(1);
         skibidii.setPower(1);
+        skibidiii.setPower(1);
+        skibidiiii.setPower(1);
+
+        while (opModeIsActive()) {
+        }
+
+        skibidi.setPower(0);
+        skibidii.setPower(0);
+        skibidiii.setPower(0);
+        skibidiiii.setPower(0);
     }
 }
