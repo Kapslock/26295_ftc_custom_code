1743b51ed1c1a1f9417c2e51ef48879778f7b5e8
reaganmitton <142063056+reaganmitton@users.noreply.github.com>
2025-09-17 11:26:26 -0400

    Update RB_Auto.java
    added basic initalization stuff

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RB_Auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RB_Auto.java
@@ -1,4 +1,30 @@
 package org.firstinspires.ftc.TeamCode;
 
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+@Autonomous(name = "RB Auto")
 public class RB_Auto {
+  
+  private DcMotor frontLeftMotor;
+  private DcMotor backLeftMotor;
+  private DcMotor frontRightMotor;
+  private DcMotor backRightMotor;
+
+  @Override
+  public void runOpMode(){
+
+    frontLeftMotor = hardwareMap.get(DcMotor.class, "frontLeftMotor");
+    backLeftMotor = hardwareMap.get(DcMotor.class, "backLeftMotor");
+    frontRightMotor = hardwareMap.get(DcMotor.class, "frontRightMotor");
+    backRightMotor = hardwareMap.get(DcMotor.class, "backRightMotor");
+    servoRotate = hardwareMap.get(CRServo.class, "servoRotate");
+
+    waitForStart();
+    
+  }
+  
 }
+
