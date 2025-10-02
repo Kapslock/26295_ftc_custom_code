86bb14c0e420ab78b59a471093c47bab170611f6
James Beers <jamesfbeers@gmail.com>
2025-10-02 16:14:26 -0700

    Added Sensors class for future sensors.
    Added runtime tracker

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/driveTrain/Sensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/driveTrain/Sensors.java
@@ -2,12 +2,10 @@ package org.firstinspires.ftc.teamcode.teleOp.driveTrain;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.hardwareMap;
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
-
 import android.hardware.HardwareBuffer;
 import android.hardware.Sensor;
 import com.qualcomm.hardware.rev.RevColorSensorV3;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,7 +13,7 @@ import org.slf4j.LoggerFactory;
 public class Sensors {
     private static final Logger log = LoggerFactory.getLogger(Sensors.class);
 
-    private RevColorSensorV3 color = (RevColorSensorV3) hardwareMap.colorSensor.get("colorV3");
+    private final RevColorSensorV3 color = (RevColorSensorV3) hardwareMap.colorSensor.get("colorV3");
 
     public void init (HardwareMap hwMap) {
 
