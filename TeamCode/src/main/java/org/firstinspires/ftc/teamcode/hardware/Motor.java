dde030749772f2621ad8a5b88b06a4598fca2554
Kolton Bloom <bloomkd46@icloud.com>
2025-10-04 18:47:59 +0000

    removed unesecarry import

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Motor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Motor.java
@@ -1,6 +1,5 @@
 package org.firstinspires.ftc.teamcode.hardware;
 
-import com.android.annotations.Nullable;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
