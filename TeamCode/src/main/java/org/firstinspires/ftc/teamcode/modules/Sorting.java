55a49b557f0849c169cc5be0827e1946984a328e
NIkitaAvd12524 <n.avdyukov@lab244.ru>
2025-10-08 19:53:27 +0300

    made constructor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Sorting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Sorting.java
@@ -1,4 +1,41 @@
 package org.firstinspires.ftc.teamcode.modules;
 
-public class Sorting  {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+
+public class Sorting {
+    private DcMotor drum;
+    private Servo wall;
+
+    public static double SPEED;
+    public static double OPEN_WALL;
+    public static double CLOSE_WALL;
+
+
+    public void runOpMode(LinearOpMode opMode) {
+        this.drum = opMode.hardwareMap.get(DcMotor.class, "drum");
+        this.wall = opMode.hardwareMap.get(Servo.class, "wall");
+        drum.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        drum.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        drum.setDirection(DcMotor.Direction.REVERSE);
+    }
+
+    public void turnDrum() {
+
+    }
+
+    public void switchWall() {
+        if (wall.getPosition() == OPEN_WALL) {
+            wall.setPosition(CLOSE_WALL);
+        } else {
+            wall.setPosition(OPEN_WALL);
+        }
+    }
+
+
 }
+
+
