3da8009ec8303b31274ba877a863272e660065cc
IndecisiveDevices <ShamRobotics@outlook.com>
2025-10-08 20:53:12 -0400

    created new teleop class to test how Servos work.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/ServoTest2025RobotCode_TeleOp.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "ServoTest2025RobotCode_TeleOp", group = "Robot")
+public class ServoTest2025RobotCode_TeleOp extends OpMode {
+
+    Servo carousel;
+    Servo kicker;
+
+    @Override
+    public void init() {
+        carousel = hardwareMap.get(Servo.class, "carousel");
+        kicker = hardwareMap.get(Servo.class, "kicker");
+
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.x) {
+            carousel.setPosition(90);
+        }
+        if(gamepad1.a) {
+            kicker.setPosition(132);
+        }
+    }
+}
