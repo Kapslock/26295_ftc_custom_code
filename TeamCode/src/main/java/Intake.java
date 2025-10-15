04ce32a2b0734727be17745964fe54fb4c305e12
BK <brandonjinukim@gmail.com>
2025-10-15 10:20:48 -0700

    AprilTag

--- TeamCode/src/main/java/Intake.java
+++ TeamCode/src/main/java/Intake.java
@@ -5,35 +5,39 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public class Intake {
-
     private final DcMotor intakeSpin;
     private final LinearOpMode linearOpMode;
 
     public Intake(LinearOpMode linearOpMode) {
-        this.intakeSpin = linearOpMode.hardwareMap.dcMotor.get("intakeSpin");
         this.linearOpMode = linearOpMode;
-
+        this.intakeSpin = linearOpMode.hardwareMap.dcMotor.get("intakeSpin");
     }
 
     public void configureMotorModes() {
         intakeSpin.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
         intakeSpin.setDirection(DcMotorSimple.Direction.FORWARD);
     }
 
-    public void inttakeArtifact(boolean intakeArtifact) {
-        if (inttakeArtifact) {
+    public void intakeArtifactSlow(boolean intakeArtifactSlow) {
+        if (intakeArtifactSlow) {
+            intakeSpin.setPower(0.2);
+            linearOpMode.sleep(2500);
+        } else if (linearOpMode.isStopRequested()) {
+            intakeSpin.setPower(0);
+        }
+    }
+
+    public void intakeArtifact(boolean intakeArtifact) {
+        if (intakeArtifact) {
             intakeSpin.setPower(0.8);
-            sleep(2500);
-        } else {
-             intakeSpin.setPower(0);
+            linearOpMode.sleep(2500);
+        } else if (linearOpMode.isStopRequested()) {
+            intakeSpin.setPower(0);
         }
     }
 
     public void displayTelemetry() {
         linearOpMode.telemetry.addData("Intake Motor Power", intakeSpin.getPower());
         linearOpMode.telemetry.update();
-
-    
     }
 }
