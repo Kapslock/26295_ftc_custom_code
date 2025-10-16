f3cdfb68b796cbac039c6948be4eae3136b8ebb5
IndecisiveDevices <ShamRobotics@outlook.com>
2025-10-16 00:56:14 -0400

    organize init() method by breaking out into private methods for each type of mechanism

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/Decode2025RobotCode_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/Decode2025RobotCode_TeleOp.java
@@ -9,15 +9,39 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 @TeleOp(name = "Decode2025RobotCode_TeleOp", group = "Robot")
 public class Decode2025RobotCode_TeleOp extends OpMode {
-    public DcMotor frontLeftDrive = null;
-    public DcMotor frontRightDrive  = null;
-    public DcMotor rearLeftDrive = null;
-    public DcMotor rearRightDrive  = null;
+    // Drive Motors
+    private DcMotor frontLeftDrive, frontRightDrive, rearLeftDrive, rearRightDrive;
 
+    // can be used to determine robot orientation (gyro)
     private IMU imu;
 
     @Override
     public void init() {
+        initDriveMotors();
+        initImu();
+        initSensors();
+        initServos();
+    }
+
+    private void initServos() {
+
+    }
+
+    private void initSensors() {
+
+    }
+
+    private void initImu() {
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD);
+
+        imu.initialize(new IMU.Parameters(RevOrientation));
+    }
+
+    private void initDriveMotors() {
         frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeft_motor");
         frontRightDrive = hardwareMap.get(DcMotor.class, "frontRight_motor");
         rearLeftDrive = hardwareMap.get(DcMotor.class, "rearLeft_motor");
@@ -30,44 +54,33 @@ public class Decode2025RobotCode_TeleOp extends OpMode {
         frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rearLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         rearRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
-        imu = hardwareMap.get(IMU.class, "imu");
-
-        RevHubOrientationOnRobot RevOrientation = new RevHubOrientationOnRobot(
-                RevHubOrientationOnRobot.LogoFacingDirection.UP,
-                RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD);
-
-        imu.initialize(new IMU.Parameters(RevOrientation));
     }
 
-    public void drive(double forward, double strafe, double rotate) {
-        double frontLeftDrive = forward + strafe + rotate;
-        double rearLeftDrive = forward - strafe + rotate;
-        double frontRightDrive = forward - strafe - rotate;
-        double rearRightDrive = forward + strafe - rotate;
+    private void drive(double forward, double strafe, double rotate) {
+        double frontLeftPower = forward + strafe + rotate;
+        double rearLeftPower = forward - strafe + rotate;
+        double frontRightPower = forward - strafe - rotate;
+        double rearRightPower = forward + strafe - rotate;
 
         double maxPower = 1.0;
-        double maxSpeed = 1.0;
+        double maxSpeed = 1.0; // this is a throttle that we can set lower for demos. Do no set higher than 1.0
 
-        maxPower = Math.max(maxPower, Math.abs(frontLeftDrive));
-        maxPower = Math.max(maxPower, Math.abs(rearLeftDrive));
-        maxPower = Math.max(maxPower, Math.abs(frontRightDrive));
-        maxPower = Math.max(maxPower, Math.abs(rearRightDrive));
+        maxPower = Math.max(maxPower, Math.abs(frontLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(rearLeftPower));
+        maxPower = Math.max(maxPower, Math.abs(frontRightPower));
+        maxPower = Math.max(maxPower, Math.abs(rearRightPower));
 
-        this.frontLeftDrive.setPower(maxSpeed * (frontLeftDrive/ maxPower));
-        this.rearRightDrive.setPower(maxSpeed * (rearRightDrive/ maxPower));
-        this.rearLeftDrive.setPower(maxSpeed * (rearLeftDrive/ maxPower));
-        this.frontRightDrive.setPower(maxSpeed * (frontRightDrive/ maxPower));
-
+        this.frontLeftDrive.setPower(maxSpeed * (frontLeftPower/ maxPower));
+        this.rearRightDrive.setPower(maxSpeed * (rearRightPower/ maxPower));
+        this.rearLeftDrive.setPower(maxSpeed * (rearLeftPower/ maxPower));
+        this.frontRightDrive.setPower(maxSpeed * (frontRightPower/ maxPower));
     }
-
-    double forward, strafe, rotate;
 
     @Override
     public void loop() {
-        forward = -gamepad1.left_stick_y;
-        strafe = gamepad1.left_stick_x;
-        rotate = gamepad1. right_stick_x;
+        double forward = -gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double rotate = gamepad1. right_stick_x;
 
         drive(forward, strafe, rotate);
     }
