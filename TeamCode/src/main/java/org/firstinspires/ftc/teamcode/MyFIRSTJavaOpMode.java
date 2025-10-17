595ace38295f21160fd6f731e8d9b4080fe258a6
Seb <sebastien.perol@gmail.com>
2025-10-17 16:55:33 +0200

    wip vision portal

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyFIRSTJavaOpMode.java
@@ -1,53 +1,84 @@
+package org.firstinspires.ftc.teamcode;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.IMU;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.vision.VisionPortal;
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
 // OpMode FTC SIM ONLY
 // ATTENTION : nommage obligatoire pour ftcsim
 // manque la @Teleop pour un vrai TeamCode
 public class MyFIRSTJavaOpMode extends LinearOpMode {
+    final double DESIRED_DISTANCE = 25.0; //  this is how close the camera should get to the target (inches)
+
+    // TODO : enum not working with ftc sim
+    static final int BLUE_TAG_ID = 20;
+    static final int RED_TAG_ID = 24;
+    static final int CENTER_TAG_ID = 21;
     DcMotor backLeftDrive;
     DcMotor backRightDrive;
     DcMotor frontLeftDrive;
     DcMotor frontRightDrive;
-    IMU imu;
 
     private void initialize() {
         backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
         backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
         frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
         frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
-        imu = hardwareMap.get(IMU.class, "imu");
+        // imu = hardwareMap.get(IMU.class, "imu"); not available sur ftc sim
 
         frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
         backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
 
-        telemetry.addData("init", "done");
+        telemetry.addData("initialize", "done");
         telemetry.update();
     }
 
 
-    private void keyboardDrive() {
-        while (opModeIsActive()) {
-            float turn = keyboard.isPressed(108) - keyboard.isPressed(106); // j et l
-            float forward = keyboard.isPressed(105) - keyboard.isPressed(107); // i et k
-            float strafe = keyboard.isPressed(111) - keyboard.isPressed(117); // o et u
-            processDriveInputs(turn, forward, strafe);
-        }
+    // TODO obligé de tenir les builders, obligé d'avois vision portal sinon pas de datas
+    VisionPortal.Builder myVisionPortalBuilder;
+    VisionPortal myVisionPortal;
+    AprilTagProcessor.Builder myAprilTagProcessorBuilder;
+    AprilTagProcessor myApriltagProcessor;
+
+    private void initializeVisionPortal() {
+        myVisionPortalBuilder = new VisionPortal.Builder();
+        myVisionPortal = (myVisionPortalBuilder.build());
+        myVisionPortalBuilder.setCamera(hardwareMap.get(WebcamName.class, "webcam"));
+        myAprilTagProcessorBuilder = new AprilTagProcessor.Builder();
+        myApriltagProcessor = (myAprilTagProcessorBuilder.build());
+        myVisionPortalBuilder.addProcessor(myApriltagProcessor);
     }
 
-    private void processDriveInputs(float turn, float forward, float strafe) {
+    private void processDriveInputs(double turn, double forward, double strafe) {
         // Combine inputs to create drive and turn (or both!)
-        float frontLeftPower = forward + turn + strafe;
-        float frontRightPower = forward + turn - strafe;
-        float backLeftPower = forward - turn + strafe;
-        float backRightPower = forward + turn + strafe;
-        telemetry.addData("processDriveInputs", "turn:" + turn + " forward:" + forward + " strafe:" + strafe);
-        telemetry.addData("frontLeftPower", frontLeftPower);
-        telemetry.addData("frontRightPower: ", frontRightPower);
-        telemetry.addData("backLeftPower: ", backLeftPower);
-        telemetry.addData("backRightPower: ", backRightPower);
-        telemetry.update();
+        double frontLeftPower = forward + turn + strafe;
+        double frontRightPower = forward + turn - strafe;
+        double backLeftPower = forward - turn + strafe;
+        double backRightPower = forward + turn + strafe;
+
+        // Normalize wheel powers to be less than 1.0
+        double max = Math.max(Math.abs(frontLeftPower), Math.abs(frontRightPower));
+        max = Math.max(max, Math.abs(backLeftPower));
+        max = Math.max(max, Math.abs(backRightPower));
+
+        if (max > 1.0) {
+            frontLeftPower /= max;
+            frontRightPower /= max;
+            backLeftPower /= max;
+            backRightPower /= max;
+        }
+
+//        telemetry.addData("processDriveInputs", "turn:" + turn + " forward:" + forward + " strafe:" + strafe);
+//        telemetry.addData("frontLeftPower", frontLeftPower);
+//        telemetry.addData("frontRightPower: ", frontRightPower);
+//        telemetry.addData("backLeftPower: ", backLeftPower);
+//        telemetry.addData("max: ", max);
+//        telemetry.update();
+
         frontLeftDrive.setPower(frontLeftPower);
         frontRightDrive.setPower(frontRightPower);
         backLeftDrive.setPower(backLeftPower);
@@ -160,8 +191,7 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         sleep(5000);
     }
 
-
-    public void autoDrive() {
+    public void demoDrive() {
         // go to the center
         slideRightFor(300);
 
@@ -184,13 +214,119 @@ public class MyFIRSTJavaOpMode extends LinearOpMode {
         keyboardDrive();
     }
 
+
+    private void keyboardDrive() {
+        // debrider ca,
+        // permettre de lancer autodrive sur une touche,
+        // de l'arreter sun une autre
+        // au depart on est en mode keyboard et on choisit de lancer l'autodrive
+        TODO
+        while (opModeIsActive()) {
+            float turn = keyboard.isPressed(108) - keyboard.isPressed(106); // j et l
+            float forward = keyboard.isPressed(105) - keyboard.isPressed(107); // i et k
+            float strafe = keyboard.isPressed(111) - keyboard.isPressed(117); // o et u
+            processDriveInputs(turn, forward, strafe);
+            displayVisionPortalData();
+        }
+    }
+
+    private void displayVisionPortalData() {
+        for (AprilTagDetection myAprilTagDetection : myApriltagProcessor.getDetections()) {
+            telemetry.addData("ID", (myAprilTagDetection.id));
+            telemetry.addData("Range", (myAprilTagDetection.ftcPose.range));
+            telemetry.addData("Yaw", (myAprilTagDetection.ftcPose.yaw));
+            telemetry.addData("Bearing", (myAprilTagDetection.ftcPose.bearing));
+        }
+        telemetry.update();
+    }
+
+    private void autoDrive() {
+        boolean autoDriveMode = false;
+        while (!autoDriveMode) {
+            displayVisionPortalData();
+            boolean found = false;
+            for (AprilTagDetection myAprilTagDetection : myApriltagProcessor.getDetections()) {
+                if (myAprilTagDetection.id == 20) {
+                    found = true;
+//                    telemetry.addData("Found", "ID %d (%s)", myAprilTagDetection.id, myAprilTagDetection.metadata.name);
+//                    telemetry.addData("Range", "%5.1f inches", myAprilTagDetection.ftcPose.range);
+//                    telemetry.addData("Bearing", "%3.0f degrees", myAprilTagDetection.ftcPose.bearing);
+//                    telemetry.addData("Yaw", "%3.0f degrees", myAprilTagDetection.ftcPose.yaw);
+//                    telemetry.update();
+                    telemetry.addData("ID", (myAprilTagDetection.id));
+                    telemetry.addData("Range", (myAprilTagDetection.ftcPose.range));
+                    telemetry.addData("Yaw", (myAprilTagDetection.ftcPose.yaw));
+                    telemetry.addData("Bearing", (myAprilTagDetection.ftcPose.bearing));
+                    telemetry.update();
+
+                    if (myAprilTagDetection.ftcPose.range < DESIRED_DISTANCE && myAprilTagDetection.ftcPose.bearing == 0 && myAprilTagDetection.ftcPose.yaw == 0) {
+                        autoDriveMode = true;
+                        telemetry.addData("END", "END");
+                        telemetry.update();
+                        break;
+                    }
+
+                    //  Set the GAIN constants to control the relationship between the measured position error, and how much power is
+                    //  applied to the drive motors to correct the error.
+                    //  Drive = Error * Gain    Make these values smaller for smoother control, or larger for a more aggressive response.
+                    final double SPEED_GAIN = 0.02;   //  Forward Speed Control "Gain". e.g. Ramp up to 50% power at a 25 inch error.   (0.50 / 25.0)
+                    final double STRAFE_GAIN = 0.015;   //  Strafe Speed Control "Gain".  e.g. Ramp up to 37% power at a 25 degree Yaw error.   (0.375 / 25.0)
+                    final double TURN_GAIN = 0.01;   //  Turn Control "Gain".  e.g. Ramp up to 25% power at a 25 degree error. (0.25 / 25.0)
+
+                    final double MAX_AUTO_SPEED = 0.5;   //  Clip the approach speed to this max value (adjust for your robot)
+                    final double MAX_AUTO_STRAFE = 0.5;   //  Clip the strafing speed to this max value (adjust for your robot)
+                    final double MAX_AUTO_TURN = 0.3;   //  Clip the turn speed to this max value (adjust for your robot)
+
+                    double rangeError = (myAprilTagDetection.ftcPose.range - DESIRED_DISTANCE);
+                    double headingError = myAprilTagDetection.ftcPose.bearing;
+                    double yawError = myAprilTagDetection.ftcPose.yaw;
+                    telemetry.addData("auto", "rangeError:" + rangeError + "- headingError:" + headingError + "- yawError:" + yawError);
+                    telemetry.update();
+                    // Use the speed and turn "gains" to calculate how we want the robot to move.
+//                    double drive = Range.clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+//                    double turn = Range.clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
+//                    double strafe = Range.clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+                    double drive = clip(rangeError * SPEED_GAIN, -MAX_AUTO_SPEED, MAX_AUTO_SPEED);
+                    double turn = clip(headingError * TURN_GAIN, -MAX_AUTO_TURN, MAX_AUTO_TURN);
+                    double strafe = clip(-yawError * STRAFE_GAIN, -MAX_AUTO_STRAFE, MAX_AUTO_STRAFE);
+                    telemetry.addData("autodrive", "drive:" + drive + " turn:" + turn + " strafe:" + strafe);
+                    telemetry.update();
+                    processDriveInputs(turn, drive, strafe);
+                    sleep(10);
+                    stopWheels();
+                    break;
+                }
+            }
+            if (!found) {
+                turnLeftFor(10);
+                sleep(50);
+                displayVisionPortalData();
+                sleep(1000);
+                // TODO mettre un garde fou apres un tour complet?
+            }
+        }
+
+    }
+
+    private double clip(double value, double min, double max) {
+        if (value < min) {
+            return min;
+        } else if (value > max) {
+            return max;
+        }
+        return value;
+    }
+
+
     @Override
     public void runOpMode() {
         initialize();
+        initializeVisionPortal();
         waitForStart();
 
-        //keyboardDrive();
+        // demoDrive();
         autoDrive();
+        //keyboardDrive();
 
     }
 
